602ba0c9370d09b7a8ead7bd51e1881558783668
krllbrsf <kirbor204@gmail.com>
2023-10-06 15:49:34 +0300

    test

---
+++ src/Lab1/Entities/IDamageable.cs
@@ -0,0 +1,9 @@
+﻿namespace Itmo.ObjectOrientedProgramming.Lab1.Entities;
+
+public interface IDamageable
+{
+    public int HealthPoints { get; }
+    public int MaxAsteroidCollisions { get; }
+    public int MaxMeteoriteCollisions { get; }
+    public void TakeDamage(IObstacle obstacle);
+}
